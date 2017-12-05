f3155680dd3cc5c2443e07ee375f2e20175dc8fb
John Maguire <john.maguire@gmail.com>
2017-12-05 06:11:25 -0600

    Add TLS to client & server

--- client/client.go
+++ client/client.go
@@ -8,6 +8,7 @@ import (
 
   "github.com/clementine/codesigner"
   "google.golang.org/grpc"
+  "google.golang.org/grpc/credentials"
 )
 
 var address = flag.String("address", "localhost:5001", "Address of CodeSigner GRPC service")
@@ -15,10 +16,15 @@ var dmg = flag.String("dmg", "", "Path to unsigned DMG")
 var developerID = flag.String("developer-id", "", "Developer ID to sign with")
 var password = flag.String("password", "", "Password for keychain containing developer ID")
 var output = flag.String("output", "clementine.dmg", "Path to output signed dmg")
+var cert = flag.String("cert", "", "Path to server TLS cert")
 
 func main() {
   flag.Parse()
-  conn, err := grpc.Dial(*address, grpc.WithInsecure(), grpc.WithDefaultCallOptions(grpc.MaxCallSendMsgSize(100*1024*1024), grpc.MaxCallRecvMsgSize(100*1024*1024)))
+  creds, err := credentials.NewClientTLSFromFile(*cert, "")
+  if err != nil {
+    log.Fatalf("Failed to load TLS cert")
+  }
+  conn, err := grpc.Dial(*address, grpc.WithTransportCredentials(creds), grpc.WithDefaultCallOptions(grpc.MaxCallSendMsgSize(100*1024*1024), grpc.MaxCallRecvMsgSize(100*1024*1024)))
   if err != nil {
     log.Fatalf("Could not connect to GRPC service: %v", err)
   }
--- server/server.go
+++ server/server.go
@@ -8,10 +8,13 @@ import (
 
   "github.com/clementine/codesigner"
   "google.golang.org/grpc"
+  "google.golang.org/grpc/credentials"
   "google.golang.org/grpc/reflection"
 )
 
 var port = flag.Int("port", 5001, "Port to start GRPC server on")
+var cert = flag.String("cert", "", "Path to TLS certificate")
+var key = flag.String("key", "", "Path to TLS key")
 
 func main() {
   flag.Parse()
@@ -19,7 +22,11 @@ func main() {
   if err != nil {
     log.Fatalf("Failed to listen on port: %d, %v", *port, err)
   }
-  s := grpc.NewServer(grpc.MaxRecvMsgSize(100*1024*1024))
+  creds, err := credentials.NewServerTLSFromFile(*cert, *key)
+  if err != nil {
+    log.Fatalf("Failed to load server credentials: %v", err)
+  }
+  s := grpc.NewServer(grpc.Creds(creds), grpc.MaxRecvMsgSize(100*1024*1024))
   codesigner.RegisterCodeSignerServer(s, &codesigner.CodeSigner{})
   reflection.Register(s)
   log.Println("Starting server...")
