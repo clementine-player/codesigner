709604a1c1cd72a0d35086eaac204b0441b36293
John Maguire <john.maguire@gmail.com>
2017-12-05 08:59:13 -0600

    gofmt

--- client/client.go
+++ client/client.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-  "context"
-  "crypto/tls"
-  "crypto/x509"
-  "flag"
-  "io/ioutil"
-  "log"
-  "net"
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"flag"
+	"io/ioutil"
+	"log"
+	"net"
 
-  "github.com/clementine/codesigner"
-  "google.golang.org/grpc"
-  "google.golang.org/grpc/credentials"
+	"github.com/clementine/codesigner"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 var address = flag.String("address", "localhost:5001", "Address of CodeSigner GRPC service")
@@ -23,48 +23,48 @@ var key = flag.String("key", "", "Path to client TLS key")
 var ca = flag.String("ca", "", "Path to CA Certificate")
 
 func main() {
-  flag.Parse()
-  crt, err := tls.LoadX509KeyPair(*cert, *key)
-  if err != nil {
-    log.Fatalf("Failed to load client TLS certificate: %v", err)
-  }
-  certPool := x509.NewCertPool()
-  caCert, err := ioutil.ReadFile(*ca)
-  if err != nil {
-    log.Fatalf("Failed to load CA certificate: %v", err)
-  }
-  ok := certPool.AppendCertsFromPEM(caCert)
-  if !ok {
-    log.Fatalf("Failed to load CA certificate: %v", err)
-  }
-  addr, _, err := net.SplitHostPort(*address)
-  if err != nil {
-    log.Fatalf("Failed to parse address flag: %s", *address)
-  }
-  creds := credentials.NewTLS(&tls.Config{
-    ServerName:   addr,
-    Certificates: []tls.Certificate{crt},
-    RootCAs:      certPool,
-  })
-  conn, err := grpc.Dial(*address, grpc.WithTransportCredentials(creds), grpc.WithDefaultCallOptions(grpc.MaxCallSendMsgSize(100*1024*1024), grpc.MaxCallRecvMsgSize(100*1024*1024)))
-  if err != nil {
-    log.Fatalf("Could not connect to GRPC service: %v", err)
-  }
-  defer conn.Close()
+	flag.Parse()
+	crt, err := tls.LoadX509KeyPair(*cert, *key)
+	if err != nil {
+		log.Fatalf("Failed to load client TLS certificate: %v", err)
+	}
+	certPool := x509.NewCertPool()
+	caCert, err := ioutil.ReadFile(*ca)
+	if err != nil {
+		log.Fatalf("Failed to load CA certificate: %v", err)
+	}
+	ok := certPool.AppendCertsFromPEM(caCert)
+	if !ok {
+		log.Fatalf("Failed to load CA certificate: %v", err)
+	}
+	addr, _, err := net.SplitHostPort(*address)
+	if err != nil {
+		log.Fatalf("Failed to parse address flag: %s", *address)
+	}
+	creds := credentials.NewTLS(&tls.Config{
+		ServerName:   addr,
+		Certificates: []tls.Certificate{crt},
+		RootCAs:      certPool,
+	})
+	conn, err := grpc.Dial(*address, grpc.WithTransportCredentials(creds), grpc.WithDefaultCallOptions(grpc.MaxCallSendMsgSize(100*1024*1024), grpc.MaxCallRecvMsgSize(100*1024*1024)))
+	if err != nil {
+		log.Fatalf("Could not connect to GRPC service: %v", err)
+	}
+	defer conn.Close()
 
-  unsigned, err := ioutil.ReadFile(*dmg)
-  if err != nil {
-    log.Fatalf("Failed to read DMG file: %v", err)
-  }
-  c := codesigner.NewCodeSignerClient(conn)
-  reply, err := c.SignPackage(context.Background(), &codesigner.SignPackageRequest{
-    Package: unsigned,
-    DeveloperId: *developerID,
-  })
-  if err != nil {
-    log.Fatalf("Failed to sign package: %v", err)
-  }
-  if err = ioutil.WriteFile(*output, reply.GetSignedPackage(), 0644); err != nil {
-    log.Fatalf("Failed to write output dmg: %v", err)
-  }
+	unsigned, err := ioutil.ReadFile(*dmg)
+	if err != nil {
+		log.Fatalf("Failed to read DMG file: %v", err)
+	}
+	c := codesigner.NewCodeSignerClient(conn)
+	reply, err := c.SignPackage(context.Background(), &codesigner.SignPackageRequest{
+		Package:     unsigned,
+		DeveloperId: *developerID,
+	})
+	if err != nil {
+		log.Fatalf("Failed to sign package: %v", err)
+	}
+	if err = ioutil.WriteFile(*output, reply.GetSignedPackage(), 0644); err != nil {
+		log.Fatalf("Failed to write output dmg: %v", err)
+	}
 }
--- codesigner_service.go
+++ codesigner_service.go
@@ -1,80 +1,80 @@
 package codesigner
 
 import (
-  "context"
-  "flag"
-  "fmt"
-  "io/ioutil"
-  "os/exec"
-  "sync"
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"sync"
 )
 
 var defaultKeychainPath = flag.String("keychain", "buildbot.keychain", "Path to keychain containing developer IDs")
 var password = flag.String("password", "", "Password for the keychain")
 
-type CodeSigner struct{
-  lock sync.Mutex
+type CodeSigner struct {
+	lock sync.Mutex
 }
 
 func unlockKeychain(password string, keychainPath string) (string, error) {
-  cmd := exec.Command("security", "unlock-keychain", "-p", password, keychainPath)
-  out, err := cmd.CombinedOutput()
-  return string(out), err
+	cmd := exec.Command("security", "unlock-keychain", "-p", password, keychainPath)
+	out, err := cmd.CombinedOutput()
+	return string(out), err
 }
 
 func lockKeychain(keychainPath string) (string, error) {
-  cmd := exec.Command("security", "lock-keychain", keychainPath)
-  out, err := cmd.CombinedOutput()
-  return string(out), err
+	cmd := exec.Command("security", "lock-keychain", keychainPath)
+	out, err := cmd.CombinedOutput()
+	return string(out), err
 }
 
 func (s *CodeSigner) SignPackage(ctx context.Context, req *SignPackageRequest) (*SignPackageReply, error) {
-  s.lock.Lock()
-  defer s.lock.Unlock()
+	s.lock.Lock()
+	defer s.lock.Unlock()
 
-  unlock, err := unlockKeychain(*password, *defaultKeychainPath)
-  if err != nil {
-    return nil, fmt.Errorf("Failed to unlock keychain: %v; %s", err, unlock)
-  }
-  defer lockKeychain(*defaultKeychainPath)
-  temp, err := ioutil.TempFile("", "codesigner")
-  if err != nil {
-    return nil, fmt.Errorf("Failed to create temp file for signing: %v", err)
-  }
-  _, err = temp.Write(req.GetPackage())
-  if err != nil {
-    return nil, fmt.Errorf("Failed to write temp file: %v", err)
-  }
-  temp.Close()
-  cmd := exec.Command("codesign", "-fv", "-s", req.GetDeveloperId(), temp.Name())
-  out, err := cmd.CombinedOutput()
-  if err != nil {
-    return nil, fmt.Errorf("Failed to codesign: %s", out)
-  }
-  signed, err := ioutil.ReadFile(temp.Name())
-  if err != nil {
-    return nil, fmt.Errorf("Failed to read back signed data: %v", err)
-  }
-  return &SignPackageReply{
-    SignedPackage: signed,
-    CodesignOutput: string(out),
-  }, nil
+	unlock, err := unlockKeychain(*password, *defaultKeychainPath)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to unlock keychain: %v; %s", err, unlock)
+	}
+	defer lockKeychain(*defaultKeychainPath)
+	temp, err := ioutil.TempFile("", "codesigner")
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create temp file for signing: %v", err)
+	}
+	_, err = temp.Write(req.GetPackage())
+	if err != nil {
+		return nil, fmt.Errorf("Failed to write temp file: %v", err)
+	}
+	temp.Close()
+	cmd := exec.Command("codesign", "-fv", "-s", req.GetDeveloperId(), temp.Name())
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to codesign: %s", out)
+	}
+	signed, err := ioutil.ReadFile(temp.Name())
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read back signed data: %v", err)
+	}
+	return &SignPackageReply{
+		SignedPackage:  signed,
+		CodesignOutput: string(out),
+	}, nil
 }
 
 func (s *CodeSigner) VerifyPackage(ctx context.Context, req *VerifyPackageRequest) (*VerifyPackageReply, error) {
-  temp, err := ioutil.TempFile("", "codesigner")
-  if err != nil {
-    return nil, fmt.Errorf("Failed to create temp file for verifying: %v", err)
-  }
-  _, err = temp.Write(req.GetPackage())
-  if err != nil {
-    return nil, fmt.Errorf("Failed to write temp file: %v", err)
-  }
-  temp.Close()
-  cmd := exec.Command("codesign", "-vvv", temp.Name())
-  out, err := cmd.CombinedOutput()
-  return &VerifyPackageReply{
-    Ok: err == nil,
-    CodesignOutput: string(out),
-  }, nil
+	temp, err := ioutil.TempFile("", "codesigner")
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create temp file for verifying: %v", err)
+	}
+	_, err = temp.Write(req.GetPackage())
+	if err != nil {
+		return nil, fmt.Errorf("Failed to write temp file: %v", err)
+	}
+	temp.Close()
+	cmd := exec.Command("codesign", "-vvv", temp.Name())
+	out, err := cmd.CombinedOutput()
+	return &VerifyPackageReply{
+		Ok:             err == nil,
+		CodesignOutput: string(out),
+	}, nil
 }
